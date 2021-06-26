74456d8dfc125b3ba30c48caa47d7b3df7328d4a
Dimosthenis Botsaris <botsaris.d@gmail.com>
2021-06-26 16:39:18 +0300

    Add users service

--- devops/aws/ecs_ec2/auto_scaling.tf
+++ devops/aws/ecs_ec2/auto_scaling.tf
@@ -19,12 +19,9 @@ data "aws_ami" "amazon_linux" {
 }
 
 resource "aws_launch_configuration" "lc" {
-  name          = "test_ecs"
+  name          = "ec2_ecs_launch_configuration"
   image_id      = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
-  lifecycle {
-    create_before_destroy = true
-  }
   iam_instance_profile        = aws_iam_instance_profile.ecs_service_role.name
   security_groups             = [aws_security_group.ec2-sg.id]
   associate_public_ip_address = false
@@ -33,20 +30,25 @@ resource "aws_launch_configuration" "lc" {
 sudo apt-get update
 sudo echo "ECS_CLUSTER=${var.project}" >> /etc/ecs/ecs.config
 EOF
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_autoscaling_group" "asg" {
-  name                      = "test-asg"
+resource "aws_autoscaling_group" "books_api_asg" {
+  name                      = "books-api-asg"
   launch_configuration      = aws_launch_configuration.lc.name
   min_size                  = 1
   max_size                  = 4
   desired_capacity          = 1
-  health_check_type         = "ELB"
-  health_check_grace_period = 300
+  force_delete              = true
+  load_balancers            = [] # Only used when NOT using ALB
+  # health_check_type         = "ELB"
+  # health_check_grace_period = 300
   vpc_zone_identifier       = aws_subnet.private.*.id
-  target_group_arns         = [aws_alb_target_group.books_api_tg.arn]
-  protect_from_scale_in     = true
-  lifecycle {
-    create_before_destroy = true
-  }
+  # target_group_arns         = [aws_alb_target_group.books_api_tg.arn]
+  # protect_from_scale_in     = true
+  # lifecycle {
+  #   create_before_destroy = true
+  # }
 }
