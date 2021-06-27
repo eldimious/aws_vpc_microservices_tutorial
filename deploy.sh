947a65a0eaa707e35ac97dc244b0d46ba5b70a30
Dimosthenis Botsaris <botsaris.d@gmail.com>
2021-06-27 10:59:05 +0300

    fix #14 - add argument to define launch type

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
+echo -n "Please enter a lauch type of ECS (ec2 OR fargate): "
+read lauch_type
+
+if [ $lauch_type != "ec2" ] && [ $lauch_type != "fargate" ]; then
+  echo "Select a valid lauch type of ECS"
+  exit 0
+fi
+
 # Deploy books API docker image
 cd ./services/booksService &&
 ./deploy_books_service.sh &&
@@ -7,5 +15,10 @@ cd ../usersService &&
 ./deploy_users_service.sh &&
 
 # Deploy to cloud host (default AWS)
-cd ../../devops/aws/ecs_fargate &&
-terraform apply
+if [ $lauch_type = "fargate" ]; then
+  cd ../../devops/aws/ecs_fargate &&
+  terraform apply
+else
+  cd ../../devops/aws/ecs_ec2 &&
+  terraform apply
+fi
