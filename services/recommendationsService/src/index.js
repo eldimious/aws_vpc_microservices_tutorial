410461c90f1ba55dd70bf93016d9cfdcd16c6e9d
Dimosthenis Botsaris <botsaris.d@gmail.com>
2021-06-27 17:33:05 +0300

    Add recommendationsService

--- services/recommendationsService/src/index.js
+++ services/recommendationsService/src/index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const helmet = require('helmet');
 const compress = require('compression')();
 const useragent = require('express-useragent');
-const fetch = require('node-fetch');
 
 const app = express();
 app.use(useragent.express());
@@ -13,18 +12,17 @@ app.use(helmet());
 app.use(compress);
 app.use(cors());
 
-app.get('/books', async (req, res, next) => {
-  console.log("Enter books route handler");
+app.get('/recommendations', async (req, res, next) => {
   return res.status(200).send({
-    data: 'Books connected.'
+    data: 'Recommendation connected.'
   });
 });
 
-app.get('/books/health-check', async (req, res, next) => {
+app.get('/recommendations/health-check', async (req, res, next) => {
   return res.status(200).send('ok');
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
     console.log(`Listening on *:${port}`);
